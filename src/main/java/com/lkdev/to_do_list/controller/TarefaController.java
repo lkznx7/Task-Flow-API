119cbd42449099ddef222ad7d6bdcd5124798f7e
Lkznx7 <lkznx7@gmail.com>
2026-02-28 11:09:52 -0300

    funcao excluir funcionando

--- src/main/java/com/lkdev/to_do_list/controller/TarefaController.java
+++ src/main/java/com/lkdev/to_do_list/controller/TarefaController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/tarefas")
-@CrossOrigin
+@CrossOrigin(origins = "*", allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
 public class TarefaController {
 
     @Autowired
